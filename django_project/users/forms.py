94beb93ce820944b9b4bcb9f4051f1c716872bac
Rahul Gupta <rg5353070@gmail.com>
2023-03-17 14:56:41 +0530

    added an update func in profile for image and other info

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,6 +1,23 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
+
+#model form is a form that will allow us to work with a specific dataModel
+
+class UserUpdateForm(forms.ModelForm):
+    # default -> required == True
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
 
 
 class UserRegisterForm(UserCreationForm):
@@ -12,3 +29,4 @@ class UserRegisterForm(UserCreationForm):
         model = User
         # the order in which the fields will be shown in the form
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from PIL import Image
 # importing our user model
 from django.contrib.auth.models import User
 # extending the functionality of our user Model
@@ -12,3 +13,18 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+#the method that gets run after our model is saved 
+    def save(self):
+#run the save method of our parent class
+        super().save()
+  #open the img of our current instance      
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+        
+        
